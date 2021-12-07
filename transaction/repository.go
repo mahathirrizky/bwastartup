338394fe090f4927f79910b1349c674b9e3fd9d6
Mahathir Rizky <mahathirrizky@gmail.com>
2021-12-07 19:19:12 +0800

    get user's transaction endpoint

--- transaction/repository.go
+++ transaction/repository.go
@@ -8,6 +8,7 @@ type repository struct{
 
 type Repository interface{
 	GetByCampignID(campaignID int) ([]Transaction, error)
+	GetByUserID(userID int) ([]Transaction, error)
 }
 
 func NewRepository(db *gorm.DB) *repository{
@@ -22,4 +23,14 @@ func (r *repository) GetByCampignID(campaignID int) ([]Transaction, error){
 		return transaction, err
 	}
 	return transaction, nil
+}
+
+func (r *repository) GetByUserID(userID int) ([]Transaction, error){
+	var transactions []Transaction
+
+	err := r.db.Preload("Campaign.CampaignImages", "campaign_images.is_primary = 1").Where("user_id = ?", userID).Order("id desc").Find(&transactions).Error
+	if err != nil{
+		return transactions, err
+	}
+	return transactions, nil
 }
--- transaction/service.go
+++ transaction/service.go
@@ -5,30 +5,39 @@ import (
 	"errors"
 )
 
-type service struct{
-	repository Repository
+type service struct {
+	repository         Repository
 	campaignRepository campaign.Repository
 }
 
-type Service interface{
+type Service interface {
 	GetTransactionByCampaignID(input GetCampaignTransactionInput) ([]Transaction, error)
+	GetTransactionByUserID(userID int) ([]Transaction, error)
 }
 
-func NewService(repository Repository, campaignRepository campaign.Repository) *service{
+func NewService(repository Repository, campaignRepository campaign.Repository) *service {
 	return &service{repository, campaignRepository}
 }
 
-func (s *service) GetTransactionByCampaignID(input GetCampaignTransactionInput) ([]Transaction, error){
-	
+func (s *service) GetTransactionByCampaignID(input GetCampaignTransactionInput) ([]Transaction, error) {
+
 	campaign, err := s.campaignRepository.FindByID(input.ID)
 	if err != nil {
 		return []Transaction{}, err
 	}
-	if campaign.UserID != input.User.ID{
-		return []Transaction{}, errors.New("Not an owner of the campaign")
+	if campaign.UserID != input.User.ID {
+		return []Transaction{}, errors.New("not an owner of the campaign")
 	}
 
 	transactions, err := s.repository.GetByCampignID(input.ID)
+	if err != nil {
+		return transactions, err
+	}
+	return transactions, nil
+}
+
+func (s *service) GetTransactionByUserID(userID int) ([]Transaction, error){
+	transactions, err := s.repository.GetByUserID(userID)
 	if err != nil {
 		return transactions, err
 	}
