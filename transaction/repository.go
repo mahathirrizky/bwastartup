b6d21085013971034390e51661bbca563cd3c7ed
Mahathir Rizky <mahathirrizky@gmail.com>
2021-12-07 18:06:29 +0800

    Campaign transaction endpoint

---
+++ transaction/repository.go
@@ -0,0 +1,25 @@
+package transaction
+
+import "gorm.io/gorm"
+
+type repository struct{
+	db *gorm.DB
+}
+
+type Repository interface{
+	GetByCampignID(campaignID int) ([]Transaction, error)
+}
+
+func NewRepository(db *gorm.DB) *repository{
+	return &repository{db}
+}
+
+func (r *repository) GetByCampignID(campaignID int) ([]Transaction, error){
+	var transaction []Transaction
+
+	err :=r.db.Preload("User").Where("campaign_id = ?", campaignID).Order("id desc").Find(&transaction).Error
+	if err != nil {
+		return transaction, err
+	}
+	return transaction, nil
+}
